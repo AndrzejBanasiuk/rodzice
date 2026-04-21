dec32dcc8f16888db2f17e3034f51a9680f551c4
AndrzejBanasiuk <165994843+AndrzejBanasiuk@users.noreply.github.com>
2026-04-21 13:12:13 +0200

    Update Tailwind CSS configuration with new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "oklch(0.645 0.246 16.439)", // Twoje kolory z projektu
+        accent: "oklch(0.712 0.194 46.666)",
+      }
+    },
   },
   plugins: [],
 }
